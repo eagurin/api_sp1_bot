fad8613dc88d8faf4f7b3c24138c884ecfacf274
Евгений Гурин <evgeny@MacBook-Air-Evgenij.local>
2020-08-21 00:03:16 +0300

    upload api_sp1_bot

--- homework.py
+++ homework.py
@@ -10,11 +10,12 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    if homework.get('status') == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,14 +23,15 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    params ={'from_date': current_timestamp}
+    homework_statuses = requests.get(URL, params=params, headers=headers)
     return homework_statuses.json()
 
 
-def send_message(message):
-    ...
-    return bot.send_message(...)
+def send_message(message): 
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    return bot.send_message(chat_id=CHAT_ID, text=message) 
 
 
 def main():
